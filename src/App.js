ba049fe7c68fb3b54ec72d055a84afdbf7689b86
Anas Mostafa <anas20chakir@outlook.com>
2021-05-31 16:01:05 +0100

    Add App wide Header Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
